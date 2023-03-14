ce4ce9c275bfd92d2b7a9c53f09e94955414d5ca
indihth <86363851+indihth@users.noreply.github.com>
2023-03-14 14:32:57 +0000

    first commit

---
+++ classes/person.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor({
+    _name,
+    _dob,
+    _height,
+    _weigth,
+    _nationality,
+    _vaccineStatus,
+    _covidStatus,
+  }) {
+    this.nationalities = ["Irish", "English", "Welsh", "Scottish", "Northern Irish"];
+    this.
+  }
+}
